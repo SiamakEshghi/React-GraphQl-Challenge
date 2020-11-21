a62fe2373d1a91fa961ac951749ca077eae5ec93
SiamakEshghi <si.eshghi@gmail.com>
2020-11-21 13:10:27 -0500

    ImageCard first commit

---
+++ src/components/Home/ImageCard/ImageCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './ImageCard.module.scss';
+import unknown from '../../../assets/unknown.jpg';
+
+const ImageCard = (props) => {
+  const { title, url } = props;
+
+  return (
+    <div className={styles.card}>
+      <img src={url || unknown} alt="" className={styles.image} />
+      <h2 className={styles.title}>
+        <span>{title || 'Artist'}</span>
+      </h2>
+    </div>
+  );
+};
+
+export default ImageCard;
