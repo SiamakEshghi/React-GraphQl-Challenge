88075027b9eeb8507b65f5daf0f762e529bbccc0
SiamakEshghi <si.eshghi@gmail.com>
2020-11-21 23:33:07 -0500

    routing to artist page from sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 // @flow
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 
+import useWindowDimensions from '../../hooks/windowDimensions';
 import Head from './Head/Head';
-import useWindowDimensions from '../../hooks/useWindowDimensions';
+import Sidebar from './Sidebar/Sidebar';
 import styles from './Layout.module.scss';
 
 type LayoutProps = {
@@ -14,6 +16,12 @@ const Layout = (props: LayoutProps): React.Node => {
   const [showSidebar, setShowSidebar] = React.useState(false);
   const { windowWidth } = useWindowDimensions();
   const { t } = useTranslation();
+  const history = useHistory();
+
+  const toArtistPage = (mbid) => {
+    setShowSidebar(false);
+    history.push(`/artist/${mbid}`);
+  };
 
   //Hide sidebar for phone devices and show if showSidebar = true
   const sidebarStyle =
@@ -31,7 +39,7 @@ const Layout = (props: LayoutProps): React.Node => {
         {showSidebar ? t('layout.closeFav') : t('layout.openFav')}
       </button>
       <div className={styles.sidebar} style={sidebarStyle}>
-        Sidebar
+        <Sidebar selectFavHandler={toArtistPage} />
       </div>
       <div className={styles.main}>{props.children}</div>
     </div>
